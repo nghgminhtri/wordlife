530a496a73d4390748327be1bf69ca977bba5551
nikk <ndp190@gmail.com>
2015-08-01 14:21:55 +0700

    Implement models

--- WordLife/models.py
+++ WordLife/models.py
@@ -1,10 +1,28 @@
 from django.db.models import *
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-class Words(Model):
-	word = CharField(max_length=200, blank = False)
-	definition = CharField(max_length=200, blank = False)
-	photo = ImageField(upload_to='photos', blank = False)
-	
-	def __str__(self):
-		return self.word
+class Word(Model):
+    word = CharField(max_length=200, blank=False)
+    definition = CharField(max_length=200, blank=False)
+    photo = ImageField(upload_to='photos', blank=False)
+
+    def __str__(self):
+        return self.word
+
+
+class WLUser(User):
+    def __init__(self):
+        super(User, self).__init__()
+
+
+class WordList(Model):
+    user = ForeignKey(WLUser)
+    words = ManyToManyField(Word, through='WordListWord', blank=True)
+    listName = CharField(max_length=200, blank=False)
+
+
+class WordListWord(Model):
+    word = ForeignKey(Word)
+    wordList = ForeignKey(WordList)
