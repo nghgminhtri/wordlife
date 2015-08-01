fe8644ac2f09de94632251776b843a4a1ee31d1b
Tri Nguyen <tri.nguyen@dirox.net>
2015-08-01 13:39:53 +0700

    edit model name

--- WordLife/models.py
+++ WordLife/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import *
 
 # Create your models here.
+class Words(Model):
+	word = CharField(max_length=200, blank = False)
+	definition = CharField(max_length=200, blank = False)
+	photo = ImageField(upload_to='photos', blank = False)
+	
+	def __str__(self):
+		return self.word
--- WordLife/views.py
+++ WordLife/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import *
 # Create your views here.
+
+	 
